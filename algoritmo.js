48d859b3c1c8d01d2f05f43ef8e1e730478c5de8
agmethjimenez <127366559+agmethjimenez@users.noreply.github.com>
2024-05-23 09:29:55 -0500

    Create algoritmo.js

---
+++ algoritmo.js
@@ -0,0 +1,19 @@
+let n_masc = [];
+let n_fem = [];
+
+
+let preguntaGenero = Number(prompt("Ingresa tu genero [1: Masculino 2:Femenino] :"));
+
+switch(preguntaGenero){
+    case 1:
+        for(let i = 0; i< 7; i++){
+            let nivelMasculino = prompt(`Nivel de hemoglobina del dia ${i+1}`);
+            n_masc =
+            if(nivelMasculino){
+                
+            }
+        }
+        break;
+    case 2:
+        break;
+}
